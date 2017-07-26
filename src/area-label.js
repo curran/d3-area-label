a28d27ebb8973dbd0f89f556c913b191b48e6645
curran <curran.kelleher@gmail.com>
2017-07-26 21:55:00 +0530

    Complete implementation of fits function

--- src/area-label.js
+++ src/area-label.js
@@ -7,22 +7,13 @@ export default function(area) {
   var x = area.x();
   var y0 = area.y0();
   var y1 = area.y1();
-  var bisectorX = d3.bisector(x);
+  var bisectorX = d3.bisector(x).right;
 
   // Returns true if there is at least one rectangle
   // of the given aspect ratio and scale
   // that fits somewhere within the area.
   function fits(data, aspect, scale) {
-    var x0,
-        x1,
-        i0,
-        i1,
-        j,
-        d,
-        top,
-        bottom,
-        ceiling,
-        floor,
+    var x0, x1, i0, i1, j, d, top, bottom, ceiling, floor,
         width = scale * aspect,
         height = scale / aspect;
 
@@ -30,40 +21,27 @@ export default function(area) {
       d = data[i0];
       x0 = x(d);
       x1 = x0 + width;
-      i1 = bisectorX.right(data, x1);
-
-      console.log("width " + width);
-      console.log("height " + height);
-      console.log("x0 " + x0);
-      console.log("x1 " + x1);
-      console.log("i0 " + i0);
-      console.log("i1 " + i1);
-
-      ceiling = Infinity;
-      floor = -Infinity;
+      i1 = bisectorX(data, x1);
+      ceiling = -Infinity;
+      floor = Infinity;
       for(j = i0; j < i1; j++) {
         d = data[j];
-        top = y0(d);
-        bottom = y1(d);
-        if(top < ceiling) {
-          ceiling = top;
-        }
-        if(bottom > floor) {
+
+        bottom = y0(d);
+        if(bottom < floor) {
           floor = bottom;
         }
+
+        top = y1(d);
+        if(top > ceiling) {
+          ceiling = top;
+        }
       }
-
-      console.log("top " + top);
-      console.log("ceiling " + ceiling);
-      console.log("bottom " + bottom);
-      console.log("floor " + floor);
-
-
-      // TODO return true if (top-bottom) <= height
-      //if(test(data, x0, x1, h
-
-      return false;
+      if ((floor - ceiling) >= height) {
+        return true;
+      }
     }
+    return false;
   }
 
   return function (data) {
