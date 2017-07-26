0d4932c7aabc51f8dba0303bb7cdb7e3dded0db5
curran <curran.kelleher@gmail.com>
2017-07-26 20:21:22 +0530

    Sketch out bits of the algorithm

--- src/area-label.js
+++ src/area-label.js
@@ -1,14 +1,65 @@
+function getAspectRatio(textNode) {
+  var box = textNode.getBBox();
+  return box.width / box.height;
+}
+
 export default function(area) {
+  var x = area.x();
+  var y0 = area.y0();
+  var y1 = area.y1();
+  var bisectorX = d3.bisector(x);
+
+  // Returns true if there is at least one rectangle
+  // of the given aspect ratio and scale
+  // that fits somewhere within the area.
+  function fits(data, aspect, scale) {
+    var x0,
+        x1,
+        i0,
+        i1,
+        j,
+        d,
+        top,
+        bottom,
+        width = scale * aspect,
+        height = scale / aspect;
+
+    for(i0 = 0; i0 < data.length; i0++) {
+      d = data[i0];
+      x0 = x(d);
+      x1 = x0 + width;
+      i1 = bisectorX.right(data, x1);
+
+      console.log("width " + width);
+      console.log("height " + height);
+      console.log("x0 " + x0);
+      console.log("x1 " + x1);
+      console.log("i0 " + i0);
+      console.log("i1 " + i1);
+
+      // TODO compute top and bottom,
+      // TODO return true if (top-bottom) <= height
+      //if(test(data, x0, x1, h
+      //top = y0(d);
+      //bottom = y1(d);
+      //j = i;
+      //while(x(d) < x1 && j < data.length) {
+      //  d = data[j]
+      //  j++;
+      //}
+
+      return false;
+    }
+  }
+
   return function (data) {
-    var x = area.x();
-    var y0 = area.y0();
-    var y1 = area.y1();
+    var aspect = getAspectRatio(this);
+    var scale = 5;
 
-    console.log(this.getBBox()) // We can measure the text aspect ratio from here.
-    console.log(data) // We can access the area polygon from here, with `area`
+    console.log(fits(data, aspect, scale));
 
-    var d = data[0];
-    console.log(x(d), y0(d), y1(d));
+    //var d = data[0];
+    //console.log(x(d), y0(d), y1(d));
 
     // This is how we could set the x, y, and font-size
     d3.select(this)
